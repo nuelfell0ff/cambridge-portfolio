a09e792ab7b307f6cf7432e3d81f3786e8ac8f64
Ajayi Obaloluwa Emmanuel <obaloluwaajayi2006@gmail.com>
2026-07-17 10:47:22 +0100

    Add ProfessionalHeadline component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React from 'react';
 import PortfolioHero from '@/components/PortfolioHero';
+import ProfessionalHeadline from '@/components/ProfessionalHeadline'
 import About from '@/components/About'; 
 
 export default function Home() {
   return (
     <main className="bg-[#0a0806] text-white min-h-screen overflow-y-auto selection:bg-[#1F7299]/30">
       <PortfolioHero />
+      <ProfessionalHeadline />
       <About />
     </main>
   );
